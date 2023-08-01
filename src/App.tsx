68759f7175dc34542bb049e34b68a6c63f6b9362
luiznaiper <luiz_naiper@hotmail.com>
2023-08-01 08:00:19 -0600

    :construction: Nesting routes

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,15 @@ import Error from './pages/Error'
 function App() {
   return (
     <BrowserRouter>
+      <nav>Our navbar</nav>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="*" element={<Error />} />
+        <Route path="/" element={<Home />}>
+          <Route path="/about" element={<About />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="*" element={<Error />} />
+        </Route>
       </Routes>
+      <footer>Our footer</footer>
     </BrowserRouter>
   )
 }
